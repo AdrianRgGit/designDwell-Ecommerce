53edde023fd4135d045b09b12372fd7f515065e7
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-04-17 10:09:36 +0200

    creado layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import Layout from "./components/Layout/Layout";
 
 const App = () => {
-  return <RouterProvider router={routes} />;
+  return <Layout children={<RouterProvider router={routes} />}></Layout>;
 };
 
 export default App;
